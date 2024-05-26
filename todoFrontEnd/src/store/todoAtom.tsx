cfd79664e8895e1ac6e4fd5f21c0a5461dffde23
Aaryan Singh <aaryansingh@Aaryans-Mini.lan>
2024-05-26 07:13:29 +0530

    updating front end

--- todoFrontEnd/src/store/todoAtom.tsx
+++ todoFrontEnd/src/store/todoAtom.tsx
@@ -2,6 +2,10 @@ import { atom } from "recoil";
 
  const todoState = atom({
     key: 'todoState', // unique ID (with respect to other atoms/selectors)
-    default: [{}], // default value (aka initial value)
+    default: [{
+      name:"",
+      description:"",
+      _id:"",
+    }], // default value (aka initial value)
   });
   export default todoState
--- todoFrontEnd/src/todoFormat.tsx
+++ todoFrontEnd/src/todoFormat.tsx
@@ -3,15 +3,20 @@ import { useRecoilState } from "recoil";
 import todoState from "./store/todoAtom";
 
 export default function TodoFormat(props:any){
-    const [_todos, settodos] = useRecoilState<any>(todoState)
-    const[inputBoxDisable,setInputBoxDisable]=useState(true)
+    const [_todos, settodos] = useRecoilState(todoState)
+    const[updatedValue,setupdatedValue]=useState(props.todo)
+    const[_inputBoxDisable,setInputBoxDisable]=useState(true)
     return <div key={Math.random()} className='flex gap-2 '>
 <div>{Object.keys(props.todo).map((a) => {
+  
                 return <div key={Math.random()} className='flex gap-2 bg-red'>
                   <div>{a}:</div>
-                  <input defaultValue={props.todo[a]}  className='bg-slate-200 rounded-xl p-1' disabled={inputBoxDisable} onChange={(e)=>{settodos((prev:any)=>{prev[props.index][a]=e.target.value;return prev})}}></input>
+                  <input defaultValue={props.todo[a]} className='bg-slate-200 rounded-xl p-1' disabled={false} onChange={
+                    (e)=>{
+                      setupdatedValue((prev:any)=>{prev[a]=e.target.value;return prev})
+                      }}></input>
                 </div>
               })}</div>
-               <button onClick={() => { setInputBoxDisable((prev:any)=>{return !prev}) }}>update todo</button>
+               <button onClick={() => { setInputBoxDisable((prev:any)=>{return !prev});settodos((prev:any)=>{const newTodo=[...prev];newTodo[props.index]=updatedValue;return newTodo}) }}>update todo</button>
               </div>
 }
