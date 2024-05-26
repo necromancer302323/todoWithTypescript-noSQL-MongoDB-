fe6629ea147bf272900561db419110430293f49a
Aaryan Singh <aaryansingh@Aaryans-Mini.lan>
2024-05-26 07:22:17 +0530

    updating frontEnd todoFormat

--- todoFrontEnd/src/todoFormat.tsx
+++ todoFrontEnd/src/todoFormat.tsx
@@ -3,7 +3,7 @@ import { useRecoilState } from "recoil";
 import todoState from "./store/todoAtom";
 
 export default function TodoFormat(props:any){
-    const [_todos, settodos] = useRecoilState(todoState)
+    const [todos, settodos] = useRecoilState(todoState)
     const[updatedValue,setupdatedValue]=useState(props.todo)
     const[_inputBoxDisable,setInputBoxDisable]=useState(true)
     return <div key={Math.random()} className='flex gap-2 '>
@@ -13,7 +13,8 @@ export default function TodoFormat(props:any){
                   <div>{a}:</div>
                   <input defaultValue={props.todo[a]} className='bg-slate-200 rounded-xl p-1' disabled={false} onChange={
                     (e)=>{
-                      setupdatedValue((prev:any)=>{prev[a]=e.target.value;return prev})
+                      setupdatedValue((prev:any)=>{ console.log(todos[todos.findIndex((e)=>{return e==props.todo})]);prev[a]==e.target.value;return prev })
+                      console.log(updatedValue)
                       }}></input>
                 </div>
               })}</div>
